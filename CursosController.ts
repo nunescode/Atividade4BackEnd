2b8ff838d85a0aa7c896bb0e09758e8fb4aa20b3
nunescode <103227499+nunescode@users.noreply.github.com>
2022-09-19 16:18:57 -0300

    Pasta Controllers

---
+++ CursosController.ts
@@ -0,0 +1,14 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Curso from "App/Models/Curso";
+
+export default class CursosController {
+    index(){
+        return Curso.all()
+    }
+    store(){
+        const dados = {nome: 'ADS', duracao: 5, modalidade: 'P'}
+    
+    return Curso.create(dados)
+    }
+}
